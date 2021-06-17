6e1128c12415fbb8c412b493628006157c43d54a
fsabirov <sabirovfarit@gmail.com>
2021-06-17 17:50:38 +0300

    add input,div

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-let inputValue = 'react';
-fetch(`https://api.github.com/search/repositories?q=${inputValue}&per_page=5`)
+let inputValue = document.querySelector('.window__input');
+
+let searchUnit = document.createElement('div');
+searchUnit.classList.add('window__searchUnit');
+inputValue.after()
+
+fetch(`https://api.github.com/search/repositories?q=${inputValue.value}&per_page=5`)
     .then(response => response.json())
     .then(data => console.log(data.items));
