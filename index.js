36f4fd9ba48ea9ae13128e9040db631eb1fb5639
goldbearman <sabirovfarit@google.com>
2021-06-20 12:40:06 +0300

    All program

--- index.js
+++ index.js
@@ -1,9 +1,83 @@
-let inputValue = document.querySelector('.window__input');
+let inputValue = document.querySelector('.drop-down_menu__input');
+let description_element = document.querySelector('.description_element');
+let description_element__item = document.querySelector('.description_element__item');
+let button_close = document.querySelector('.close-button');
+let searchUnits;
 
-let searchUnit = document.createElement('div');
-searchUnit.classList.add('window__searchUnit');
-inputValue.after()
+const debounce = (fn, debounceTime) => {
+    let timeOut;
+    return function () {
+        const fnCall = () => {
+            fn.apply(this, arguments)
+        }
+        clearTimeout(timeOut);
+        timeOut = setTimeout(fnCall, debounceTime);
+    }
+};
 
-fetch(`https://api.github.com/search/repositories?q=${inputValue.value}&per_page=5`)
-    .then(response => response.json())
-    .then(data => console.log(data.items));
+
+inputValue.addEventListener('keyup', debounce(networkRequest,500) );
+
+
+
+
+function networkRequest() {
+    if (inputValue.value.trim() !== "") {
+        console.log(inputValue.value + "  first");
+        fetch(`https://api.github.com/search/repositories?q=${inputValue.value}&per_page=5&sort=stars`)
+            .then(response => {
+                    console.log("resp")
+                    if (response.ok) {
+                        return response.json();
+                    }
+                }
+            )
+            .then(data => {
+                console.log("dat")
+                if (data !== undefined) {
+
+                    createItem(data.items);
+                }
+                ;
+            });
+    } else searchUnits.remove();
+
+}
+
+function createItem(arrItems) {
+    if (searchUnits !== undefined) {
+        searchUnits.remove();
+    }
+    if (arrItems !== undefined) {
+        searchUnits = document.createElement('div');
+
+        arrItems.forEach(item => {
+            let searchUnit = document.createElement('div');
+            searchUnit.classList.add('drop-down_menu__searchUnit');
+            searchUnit.innerHTML = item.name;
+            searchUnits.append(searchUnit);
+            searchUnit.addEventListener('click', () => createDescription(item));
+        });
+        inputValue.after(searchUnits);
+    }
+}
+
+function createDescription(objectItem) {
+    inputValue.value = "";
+    if (searchUnits !== undefined) {
+        searchUnits.remove();
+    }
+    let item = description_element__item.cloneNode(true);
+    item.style.display = 'block';
+    let name = item.querySelector('p');
+    let button = item.querySelector('button');
+    name.innerHTML = `Name: ${objectItem.name} <br>
+    Owner: ${objectItem.owner.login} <br>
+    Stars: ${objectItem.stargazers_count}`;
+    button.addEventListener('click', () => item.remove());
+    description_element.prepend(item);
+}
+
+
+
+
